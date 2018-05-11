2662e6b0de4c57849bd5bd21184f6552e9d977ea
grzeprza <gprzadka@interia.pl>
2018-05-11 18:39:47 +0200

    Revert "Migrate Publisher to lombok"
    This reverts commit 69dd56d15e334e37a4c3fd15e139916bbe8cfc32.

--- src/main/java/com/grzeprza/funlib/funlib/model/Publisher.java
+++ src/main/java/com/grzeprza/funlib/funlib/model/Publisher.java
@@ -1,16 +1,12 @@
 package com.grzeprza.funlib.funlib.model;
 
-import lombok.*;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-@Data
 @Entity
-@ToString
 public class Publisher {
 
     @Id
@@ -27,6 +23,36 @@ public class Publisher {
     public Publisher(String name, Set<Book> books) {
         this.name = name;
         this.books = books;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Publisher publisher = (Publisher) o;
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
     }
 
 }
