c57f0e5092100d972d063f466f432b5c6da0570a
grzeprza <gprzadka@interia.pl>
2018-05-11 18:50:35 +0200

    Add Slf4j logger to LibraryServiceImpl

--- src/main/java/com/grzeprza/funlib/funlib/service/LibraryServiceImpl.java
+++ src/main/java/com/grzeprza/funlib/funlib/service/LibraryServiceImpl.java
@@ -7,10 +7,12 @@ import com.grzeprza.funlib.funlib.model.Publisher;
 import com.grzeprza.funlib.funlib.repository.AuthorRepository;
 import com.grzeprza.funlib.funlib.repository.BookRepository;
 import com.grzeprza.funlib.funlib.repository.PublisherRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class LibraryServiceImpl implements LibraryService{
 
     @Autowired
@@ -22,8 +24,8 @@ public class LibraryServiceImpl implements LibraryService{
     @Autowired
     PublisherRepository publisherRepository;
 
-
     public boolean addBook(Book book, String authorName, String authorSurname, String publisher){
+        log.debug("Start adding book " + book.getTitle() + " " + authorSurname + " " + publisher);
         Publisher pub = publisherRepository.findByName(publisher);
         if(pub == null) {
             pub = new Publisher();
@@ -37,7 +39,7 @@ public class LibraryServiceImpl implements LibraryService{
         }
 
         //Eric
-        if( bookRepo.findByTitle(book.getTitle()) != null ){ System.out.println("Book already exists - " + book.getTitle() + " " + authorSurname + " " + publisher); return false;}
+        if( bookRepo.findByTitle(book.getTitle()) != null ){ log.debug("Book already exists - " + book.getTitle() + " " + authorSurname + " " + publisher); return false;}
         else {
             Book newBook = book;
             newBook.setPublisher(pub);
@@ -49,6 +51,8 @@ public class LibraryServiceImpl implements LibraryService{
 
             authorRepo.save(aut);
             bookRepo.save(book);
+
+            log.debug("Book added successfully");
             return true;
         }
 
