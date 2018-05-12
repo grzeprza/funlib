a87e027e4b504855e2d52a8828174d659582136f
grzeprza <gprzadka@interia.pl>
2018-05-12 09:33:58 +0200

    Add indexController

---
+++ src/main/java/com/grzeprza/funlib/funlib/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.grzeprza.funlib.funlib.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping(value = {"/", "/index"})
+public class IndexController {
+
+    public Model index(Model model){
+        return model;
+    }
+
+}
