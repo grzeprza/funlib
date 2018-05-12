6d181b356e27de0e27c629ae392a094b59ea62b2
grzeprza <gprzadka@interia.pl>
2018-05-12 14:20:18 +0200

    Test IndexController with SpringMockMVC

--- src/test/java/com/grzeprza/funlib/funlib/controller/IndexControllerTest.java
+++ src/test/java/com/grzeprza/funlib/funlib/controller/IndexControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.LinkedList;
@@ -14,6 +16,9 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -54,5 +59,14 @@ public class IndexControllerTest {
         verify(model, times(1)).addAttribute(eq("allBooks"), listArgumentCaptor.capture());
         List<Book> booksList = listArgumentCaptor.getValue();
         assertEquals(2, booksList.size());
+    }
+
+    @Test
+    public void testMockMVC() throws Exception{
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 }
