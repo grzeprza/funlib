c68edbe9cd6154731c987025b33e2d262c0361ce
grzeprza <gprzadka@interia.pl>
2018-05-12 13:42:49 +0200

    Add BookService Implementation class

---
+++ src/main/java/com/grzeprza/funlib/funlib/service/BookServiceImpl.java
@@ -0,0 +1,26 @@
+package com.grzeprza.funlib.funlib.service;
+
+import com.grzeprza.funlib.funlib.model.Book;
+import com.grzeprza.funlib.funlib.repository.BookRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Slf4j
+@Service
+public class BookServiceImpl implements BookService{
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public List<Book> findAllBooks() {
+        List<Book> books = new LinkedList<>();
+        bookRepository.findAll().forEach(books::add);
+        log.debug(String.format("Found %d books", books.size()));
+        return books;
+    }
+}
