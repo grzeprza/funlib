6557ba8dda1d110798394436a2adcde77353586e
grzeprza <gprzadka@interia.pl>
2018-05-12 13:41:19 +0200

    Add interface BookService

---
+++ src/main/java/com/grzeprza/funlib/funlib/service/BookService.java
@@ -0,0 +1,11 @@
+package com.grzeprza.funlib.funlib.service;
+
+import com.grzeprza.funlib.funlib.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+
+    List<Book> findAllBooks();
+
+}
