2a3991912be5161c8bfac3ffde2ab91fb4c4248c
grzeprza <gprzadka@interia.pl>
2018-05-12 13:47:03 +0200

    Display books in index page

--- src/main/java/com/grzeprza/funlib/funlib/controller/BookController.java
+++ src/main/java/com/grzeprza/funlib/funlib/controller/BookController.java
@@ -1,12 +1,14 @@
 package com.grzeprza.funlib.funlib.controller;
 
 import com.grzeprza.funlib.funlib.repository.BookRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class BookController {
 
     @Autowired
@@ -14,6 +16,7 @@ public class BookController {
 
     @RequestMapping("/books")
     public String getAllBooks(Model model){
+
         model.addAttribute("books", bookRepository.findAll());
 
         return "books";
--- src/main/java/com/grzeprza/funlib/funlib/controller/IndexController.java
+++ src/main/java/com/grzeprza/funlib/funlib/controller/IndexController.java
@@ -1,16 +1,22 @@
 package com.grzeprza.funlib.funlib.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import com.grzeprza.funlib.funlib.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@Slf4j
-@RequestMapping(value = {"", "/", "/index"})
 public class IndexController {
 
+    @Autowired
+    private BookService bookService;
+
+    @RequestMapping({"/", "", "/index"})
     public String index(Model model){
+
+        model.addAttribute("allBooks", bookService.findAllBooks());
+
         return "index";
     }
 
