d6a91eb356c66de12464153ad75a0fc29fcc9907
grzeprza <gprzadka@interia.pl>
2018-05-14 23:10:44 +0200

    Display author details

--- src/main/java/com/grzeprza/funlib/funlib/controller/AuthorController.java
+++ src/main/java/com/grzeprza/funlib/funlib/controller/AuthorController.java
@@ -5,19 +5,30 @@ import com.grzeprza.funlib.funlib.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthorController {
 
-    @Autowired
     private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
 
     @RequestMapping("/authors")
     public String getAllBooks(Model model){
         model.addAttribute("authors", authorRepository.findAll());
 
         return "authors";
+    }
+
+    @RequestMapping("/authors/{name}/{surname}")
+    public String getAuthorByNameAndSurname(Model model, @PathVariable String name, @PathVariable String surname){
+        model.addAttribute("author", authorRepository.findByNameAndSurname(name, surname));
+
+        return "author";
     }
 
 }
