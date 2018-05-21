724a540fc585fac2883a3ee1a2fc424ee060d8b8
grzeprza <gprzadka@interia.pl>
2018-05-21 20:23:38 +0200

    Display new Author details page

--- src/main/java/com/grzeprza/funlib/funlib/service/LibraryServiceImpl.java
+++ src/main/java/com/grzeprza/funlib/funlib/service/LibraryServiceImpl.java
@@ -10,8 +10,14 @@ import com.grzeprza.funlib.funlib.repository.PublisherRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sun.misc.IOUtils;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Service
 @Slf4j
@@ -39,6 +45,18 @@ public class LibraryServiceImpl implements LibraryService{
         Author aut = authorRepo.findByNameAndSurname(authorName, authorSurname);
         if(aut == null){
             aut = new Author(authorName, authorSurname);
+            aut.setBirthDate(LocalDate.now());
+            aut.setDeathDate(LocalDate.now());
+            aut.setPersonalWebsiteURI(("www.about.me"));
+            Integer result = authorName.hashCode() < 0 ? authorName.hashCode()*(-1) : authorName.hashCode();
+            aut.setRef_nr(result.longValue());
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream("static/authorphoto.jpg");
+            try {
+                aut.setPicture(IOUtils.readFully(is, Integer.MAX_VALUE, false));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            aut.setPicture(null);
         }
 
         //Eric
